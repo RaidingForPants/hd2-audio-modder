3864d22284af64d61b41c17ce3869495af652397
Dekr0 <dekr0.dk@protonmail.com>
2024-10-28 19:50:07 -0600

    bugs fixes on build scripts

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ pyinstaller .\audio_modder.py --onefile
 
 cp .\azure.tcl .\dist
 cp -r .\theme .\dist
+cp -r .\AudioConversionTemplate .\dist
 
 wget -Uri "https://github.com/Dekr0/hd2_audio_db/releases/download/v.0.0.1-alpha/hd_audio_db.db" -OutFile .\dist\hd_audio_db.db
 wget -Uri "https://github.com/vgmstream/vgmstream-releases/releases/download/nightly/vgmstream-win64.zip" -OutFile .\dist\vgmstream-win64.zip
