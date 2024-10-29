8366c33f566deed9f63929fd62f9a9283ffc4a91
Dekr0 <dekr0.dk@protonmail.com>
2024-10-28 18:30:34 -0600

    script for setting up building environment

---
+++ configure.ps1
@@ -0,0 +1,7 @@
+python -m venv .venv
+
+. .\.venv\Scripts\Activate.ps1
+
+pip install -r requirements.txt
+
+deactivate
---
+++ configure.sh
@@ -0,0 +1,7 @@
+python3 -m venv .venv
+
+source ./.venv/activate
+
+pip install -r requirements.txt
+
+deactivate
