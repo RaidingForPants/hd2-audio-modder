128acd1acfba222e224c3a7a82bafeb819a73385
Evie <ben.is.not.short@gmail.com>
2024-12-28 22:47:24 -0500

    Set icon (#40)
    * Set icon
    
    * Check if icon exists before using
    
    ---------
    
    Co-authored-by: evie <evieshort65@gmail.com>

--- audio_modder.py
+++ audio_modder.py
@@ -3356,6 +3356,8 @@ class MainWindow:
         self.watched_paths = []
         
         self.root = TkinterDnD.Tk()
+        if os.path.exists("icon.ico"):
+            self.root.iconbitmap("icon.ico")
         
         self.drag_source_widget = None
         self.workspace_selection = []
