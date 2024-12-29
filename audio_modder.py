cefccc6db3e963f6b0ca1f6031b7be569980d5d8
Evie <ben.is.not.short@gmail.com>
2024-12-28 22:49:08 -0500

    Targeted import (#43)
    * Drop Import will now do targeted import if the audio source was not found. If a valid name was found, will ask if you want to do targeted import or not
    
    * Setting up for right click import
    
    * Implement targeted import function. Add filetype limiters to some filedialog windows. Allow for copying ids of non-audio source types
    
    ---------
    
    Co-authored-by: evie <evieshort65@gmail.com>

--- audio_modder.py
+++ audio_modder.py
@@ -23,6 +23,7 @@ from tkinter import filedialog
 from tkinter.messagebox import askokcancel
 from tkinter.messagebox import showwarning
 from tkinter.messagebox import showerror
+from tkinter.messagebox import askyesnocancel
 from tkinter.filedialog import askopenfilename
 from typing import Any, Literal, Callable, Union
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
@@ -2075,7 +2076,7 @@ class FileHandler:
 
     def load_wems(self, wems: Union[tuple[str, ...], Literal[""], None] = None, set_duration=True): 
         if wems == None:
-            wems = filedialog.askopenfilenames(title="Choose .wem files to import")
+            wems = filedialog.askopenfilenames(title="Choose .wem files to import", filetypes=[("Wwise Vorbis", "*.wem")])
         if wems == "":
             return
         progress_window = ProgressWindow(title="Loading Files", 
@@ -2150,7 +2151,7 @@ class FileHandler:
         
     def load_wavs(self, wavs: list[str] | None = None):
         if wavs == None:
-            wavs = filedialog.askopenfilenames(title="Choose .wav files to import")
+            wavs = filedialog.askopenfilenames(title="Choose .wav files to import", filetypes=[("WAV audio", "*.wav")])
         if wavs == "":
             return
             
@@ -3623,13 +3624,39 @@ class MainWindow:
         self.workspace_selection = self.workspace.selection()
 
     def drop_import(self, event):
+        self.drag_source_widget = None
+        renamed = False
+        old_name = ""
         if event.data:
             import_files = []
             dropped_files = event.widget.tk.splitlist(event.data)
             for file in dropped_files:
                 import_files.extend(list_files_recursive(file))
+            if (
+                len(import_files) == 1 
+                and os.path.splitext(import_files[0])[1] in [".mp3", ".wav", ".ogg", ".m4a", ".wem"]
+                and self.treeview.item(event.widget.identify_row(event.y_root - self.treeview.winfo_rooty()), option="values")[0] == "Audio Source"
+            ):
+                audio_id = self.file_handler.get_number_prefix(os.path.basename(import_files[0]))
+                if self.file_handler.get_audio_by_id(audio_id) is not None:
+                    answer = askyesnocancel(title="Import", message="There is a file with the same name, would you like to replace that instead?")
+                    if answer is None:
+                        return
+                    if not answer:
+                        new_name = f"{os.path.join(CACHE, self.treeview.item(event.widget.identify_row(event.y_root - self.treeview.winfo_rooty()), option='tags')[0])}{os.path.splitext(import_files[0])[1]}"
+                        os.rename(import_files[0], new_name)
+                        old_name = import_files[0]
+                        import_files[0] = new_name
+                        renamed = True
+                else:
+                    new_name = f"{os.path.join(CACHE, self.treeview.item(event.widget.identify_row(event.y_root - self.treeview.winfo_rooty()), option='tags')[0])}{os.path.splitext(import_files[0])[1]}"
+                    os.rename(import_files[0], new_name)
+                    old_name = import_files[0]
+                    import_files[0] = new_name
+                    renamed = True
             self.import_files(import_files)
-        self.drag_source_widget = None
+            if renamed:
+                os.rename(new_name, old_name)
 
     def drop_add_to_workspace(self, event):
         if self.drag_source_widget is not self.workspace and event.data:
@@ -3937,6 +3964,21 @@ class MainWindow:
         self.archive_search.set_entries(entries)
         self.archive_search.focus_set()
         self.category_search.selection_clear()
+        
+    def targeted_import(self, target):
+        if os.path.exists(WWISE_CLI):
+            available_filetypes = [("Audio Files", "*.wem *.wav *.mp3 *.ogg *.m4a")]
+        else:
+            available_filetypes = [("Wwise Vorbis", "*.wem")]
+        filename = askopenfilename(title="Select audio file to import", filetypes=available_filetypes)
+        if not filename or not os.path.exists(filename):
+            return
+        old_name = filename
+        new_name = f"{os.path.join(CACHE, str(target))}{os.path.splitext(filename)[1]}"
+        os.rename(filename, new_name)
+        self.import_files([new_name])
+        os.rename(new_name, old_name)
+        
 
     def treeview_on_right_click(self, event):
         try:
@@ -3958,34 +4000,38 @@ class MainWindow:
                 command=self.copy_id
             )
 
-            if not all_audio:
-                return
+            if all_audio:
+                if is_single:
+                    self.right_click_menu.add_command(
+                        label="Import audio",
+                        command=lambda: self.targeted_import(target=self.treeview.item(select, option="tags")[0])
+                    )
 
-            tags = self.treeview.item(selects[-1], option="tags")
-            assert(len(tags) == 1)
-            self.right_click_id = int(tags[0])
-            
-            self.right_click_menu.add_command(
-                label=("Dump As .wem" if is_single else "Dump Selected As .wem"),
-                command=self.dump_as_wem
-            )
-            if os.path.exists(VGMSTREAM):
+                tags = self.treeview.item(selects[-1], option="tags")
+                assert(len(tags) == 1)
+                self.right_click_id = int(tags[0])
+                
                 self.right_click_menu.add_command(
-                    label=("Dump As .wav" if is_single else "Dump Selected As .wav"),
-                    command=self.dump_as_wav,
+                    label=("Dump As .wem" if is_single else "Dump Selected As .wem"),
+                    command=self.dump_as_wem
+                )
+                if os.path.exists(VGMSTREAM):
+                    self.right_click_menu.add_command(
+                        label=("Dump As .wav" if is_single else "Dump Selected As .wav"),
+                        command=self.dump_as_wav,
+                    )
+                    self.right_click_menu.add_command(
+                        label="Dump As .wav with Sequence Number",
+                        command=lambda: self.dump_as_wav(with_seq=True)
+                    )
+                self.right_click_menu.add_command(
+                    label="Dump muted .wav with same ID",
+                    command=lambda: self.dump_as_wav(muted=True)
                 )
                 self.right_click_menu.add_command(
-                    label="Dump As .wav with Sequence Number",
-                    command=lambda: self.dump_as_wav(with_seq=True)
+                    label="Dump muted .wav with same ID and sequence number",
+                    command=lambda: self.dump_as_wav(muted=True, with_seq=True)
                 )
-            self.right_click_menu.add_command(
-                label="Dump muted .wav with same ID",
-                command=lambda: self.dump_as_wav(muted=True)
-            )
-            self.right_click_menu.add_command(
-                label="Dump muted .wav with same ID and sequence number",
-                command=lambda: self.dump_as_wav(muted=True, with_seq=True)
-            )
             self.right_click_menu.tk_popup(event.x_root, event.y_root)
         except (AttributeError, IndexError):
             pass
