903a15d41ea42a9ef45dc08166a19ec73468f8c1
Dekr0 <dekr0.dk@protonmail.com>
2025-02-02 00:25:06 -0700

    test entry point

--- testbench.py
+++ testbench.py
@@ -1,7 +1,13 @@
+import os
+import shutil
 import unittest
 
-from tests.mediautil_test import TestMediaUtil
+from env import TMP
+# from tests.mediautil_test import TestMediaUtil
+from tests.mod_test import TestMod
 
 
 if __name__ == "__main__":
+    os.mkdir(TMP)
     unittest.main()
+    shutil.rmtree(TMP)
