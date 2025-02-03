a04df0a3fbf38cd73e37415b3eb77594edfc8ecf
Dekr0 <dekr0.dk@protonmail.com>
2025-02-03 02:19:30 -0700

    testing sound_handler_test.py

--- testbench.py
+++ testbench.py
@@ -4,10 +4,12 @@ import unittest
 
 from env import TMP
 # from tests.mediautil_test import TestMediaUtil
-from tests.mod_test import TestMod
+# from tests.mod_test import TestMod
+from tests.sound_handler_test import TestSoundHandler
 
 
 if __name__ == "__main__":
+    if os.path.exists(TMP):
+        shutil.rmtree(TMP)
     os.mkdir(TMP)
     unittest.main()
-    shutil.rmtree(TMP)
