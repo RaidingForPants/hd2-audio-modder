811b0b9ddb623868e40f2c5547f49207629fb5a1
Dekr0 <dekr0.dk@protonmail.com>
2025-02-14 05:53:52 -0700

    - Give unit test variable more sane names

--- tests/cntr_parser_test.py
+++ tests/cntr_parser_test.py
@@ -1,11 +1,9 @@
 import os
-import posixpath as xpath
 import unittest
 
 import env
 
 from core import GameArchive
-from log import logger
 from wwise_hierarchy import RandomSequenceContainer, pack_cntr
 
 
@@ -21,7 +19,6 @@ class TestCntrParser(unittest.TestCase):
             name, ext = os.path.splitext(file.path)
             if ext != ".stream":
                 continue
-            logger.critical(f"Testing {os.path.basename(name)}")
             GameArchive.from_file(name)
 
     def test_robust(self):
@@ -33,30 +30,34 @@ class TestCntrParser(unittest.TestCase):
             name, ext = os.path.splitext(file.path)
             if ext != ".stream":
                 continue
-            logger.critical(f"Testing {os.path.basename(name)}")
-            left = GameArchive.from_file(name)
+
+            ctrl_group = GameArchive.from_file(name)
+
             os.environ["TEST"] = "1"
-            right = GameArchive.from_file(name)
+            exp_group = GameArchive.from_file(name)
+
             os.environ["TEST"] = "0"
 
-            left_cntrs: dict[int, RandomSequenceContainer] = {}
-            for v in left.wwise_banks.values():
-                if v.hierarchy == None:
+            ctrl_cntrs: dict[int, RandomSequenceContainer] = {}
+            for bank in ctrl_group.wwise_banks.values():
+                if bank.hierarchy == None:
                     continue
-                left_cntrs.update({
-                    k: v 
-                    for k, v in v.hierarchy.entries.items()
-                    if isinstance(v, RandomSequenceContainer)
+                ctrl_cntrs.update({
+                    hirc_entry_id: hirc_entry
+                    for hirc_entry_id, hirc_entry in bank.hierarchy.entries.items()
+                    if isinstance(hirc_entry, RandomSequenceContainer)
                 }) 
-            right_cntrs: dict[int, RandomSequenceContainer] = {}
-            for v in right.wwise_banks.values():
-                if v.hierarchy == None:
+
+            exp_cntrs: dict[int, RandomSequenceContainer] = {}
+            for bank in exp_group.wwise_banks.values():
+                if bank.hierarchy == None:
                     continue
-                right_cntrs.update({
-                    k: v 
-                    for k, v in v.hierarchy.entries.items()
-                    if isinstance(v, RandomSequenceContainer)
+                exp_cntrs.update({
+                    hirc_entry_id: hirc_entry 
+                    for hirc_entry_id, hirc_entry in bank.hierarchy.entries.items()
+                    if isinstance(hirc_entry, RandomSequenceContainer)
                 }) 
-            for k, v in right_cntrs.items():
-                self.assertTrue(k in left_cntrs)
-                self.assertEqual(pack_cntr(v), left_cntrs[k].get_data())
+
+            for exp_cntr_id, exp_cntr in exp_cntrs.items():
+                self.assertTrue(exp_cntr_id in ctrl_cntrs)
+                self.assertEqual(pack_cntr(exp_cntr), ctrl_cntrs[exp_cntr_id].get_data())
