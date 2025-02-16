6d34c14c2211e5ab179d7331401111c8cd3d3b37
Dekr0 <dekr0.dk@protonmail.com>
2025-02-16 05:29:32 -0700

    rework configuration / setting file

---
+++ setting.py
@@ -0,0 +1,47 @@
+import os
+import pickle
+
+from log import logger
+
+
+class Setting:
+
+    def __init__(self, data: str = ""):
+        self.data = data
+
+    def save(self, config_path: str = "setting.pickle"):
+        """
+        @exception
+        - OSError
+        - pickle.PickleError
+        """
+        with open(config_path, "wb") as f:
+            pickle.dump(self, f)
+
+
+def load_setting(path: str = "setting.pickle") -> Setting:
+    """
+    @exception
+    - OSError
+    - pickle.PickleError
+    """
+    if not os.path.exists(path):
+        logger.warning("Failed to locate existing application setting. Creating "
+                       " new one...")
+
+        setting = Setting()
+        setting.save()
+
+        logger.info("Created brand new application setting")
+
+        return setting
+
+    with open(path, "rb") as f:
+        setting = pickle.load(f)
+        if not isinstance(setting, Setting):
+            raise ValueError("De-serializing pickle data is not an instance of "
+                             "Setting.")
+
+        setting.save()
+
+        return setting
