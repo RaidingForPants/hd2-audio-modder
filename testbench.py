70d285eebf9623843275cf847023606afc85e884
Dekr0 <dekr0.dk@protonmail.com>
2025-02-15 22:24:34 -0700

    update test entry point for new parser tests

--- testbench.py
+++ testbench.py
@@ -1,6 +1,8 @@
 import unittest
 
 from tests.cntr_parser_test import TestCntrParser
+# from tests.actor_mixer_test import TestActorMixerParser
+# from tests.parser_integration_test import TestParserIntegration
 
 
 if __name__ == "__main__":
