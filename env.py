6d908c7dd2cd1dd8b8fb75f1ab592f2aa841245e
Dekr0 <dekr0.dk@protonmail.com>
2025-02-17 06:16:52 -0700

    remove all testing enviromental variables

--- env.py
+++ env.py
@@ -27,12 +27,6 @@ SYS_CLIPBOARD = ""
 WWISE_CLI = ""
 WWISE_VERSION = ""
 
-os.environ["TEST_ACTOR_MIXER"] = "0"
-os.environ["TEST_LAYER"] = "0"
-os.environ["TEST_RAND"] = "0"
-os.environ["TEST_SOUND"] = "0"
-os.environ["TEST_BUILD"] = "0"
-
 match SYSTEM:
     case "Windows":
         FFMPEG = "ffmpeg.exe"
