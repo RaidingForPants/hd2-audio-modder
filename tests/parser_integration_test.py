0478b83d8f4354d28e996d25ca16d5ef8e6c4766
Dekr0 <dekr0.dk@protonmail.com>
2025-02-17 01:07:10 -0700

    integrate new sound parser

--- tests/parser_integration_test.py
+++ tests/parser_integration_test.py
@@ -4,7 +4,7 @@ import unittest
 from core import GameArchive
 from log import logger
 from tests.parser_test_common import test_all_archive_sync
-from wwise_hierarchy import ActorMixer, HircEntry, LayerContainer, RandomSequenceContainer
+from wwise_hierarchy import ActorMixer, HircEntry, LayerContainer, RandomSequenceContainer, Sound, pack_sound
 from wwise_hierarchy import pack_rand_seq_cntr
 
 
@@ -14,6 +14,7 @@ class TestParserIntegration(unittest.TestCase):
         os.environ["TEST_ACTOR_MIXER"] = "0"
         os.environ["TEST_LAYER"] = "0"
         os.environ["TEST_RAND"] = "0"
+        os.environ["TEST_SOUND"] = "0"
         ctrl_group = GameArchive.from_file(archive)
         ctrl_entries: dict[int, HircEntry] = {}
         for bank in ctrl_group.wwise_banks.values():
@@ -22,7 +23,8 @@ class TestParserIntegration(unittest.TestCase):
             ctrl_entries.update({
                 hirc_entry_id: hirc_entry
                 for hirc_entry_id, hirc_entry in bank.hierarchy.entries.items()
-                if hirc_entry.hierarchy_type == 0x05 or 
+                if hirc_entry.hierarchy_type == 0x02 or
+                   hirc_entry.hierarchy_type == 0x05 or 
                    hirc_entry.hierarchy_type == 0x07 or 
                    hirc_entry.hierarchy_type == 0x09
             })
@@ -31,6 +33,7 @@ class TestParserIntegration(unittest.TestCase):
         os.environ["TEST_ACTOR_MIXER"] = "1"
         os.environ["TEST_LAYER"] = "1"
         os.environ["TEST_RAND"] = "1"
+        os.environ["TEST_SOUND"] = "1"
         exp_group = GameArchive.from_file(archive)
         exp_entries: dict[int, HircEntry] = {}
         for bank in exp_group.wwise_banks.values():
@@ -39,14 +42,20 @@ class TestParserIntegration(unittest.TestCase):
             exp_entries.update({
                 hirc_entry_id: hirc_entry
                 for hirc_entry_id, hirc_entry in bank.hierarchy.entries.items()
-                if hirc_entry.hierarchy_type == 0x05 or 
+                if hirc_entry.hierarchy_type == 0x02 or
+                   hirc_entry.hierarchy_type == 0x05 or 
                    hirc_entry.hierarchy_type == 0x07 or 
                    hirc_entry.hierarchy_type == 0x09
             })
 
         for hirc_entry_id, hirc_entry in exp_entries.items():
             self.assertIn(hirc_entry_id, ctrl_entries)
-            if isinstance(hirc_entry, RandomSequenceContainer):
+            if isinstance(hirc_entry, Sound):
+                self.assertEqual(
+                    pack_sound(hirc_entry),
+                    ctrl_entries[hirc_entry_id].get_data()
+                )
+            elif isinstance(hirc_entry, RandomSequenceContainer):
                 self.assertEqual(
                     pack_rand_seq_cntr(hirc_entry),
                     ctrl_entries[hirc_entry_id].get_data()
