68396f59cbaebd78a33fd65fbf40d25419d90be8
Dekr0 <dekr0.dk@protonmail.com>
2025-02-28 06:22:05 -0700

    test for switch container parser

--- testbench.py
+++ testbench.py
@@ -3,7 +3,8 @@ import unittest
 # from tests.actor_mixer_test import TestActorMixerParser
 # from tests.sound_parser_test import TestSoundParser
 # from tests.cntr_parser_test import TestCntrParser
-from tests.parser_integration_test import TestParserIntegration
+# from tests.parser_integration_test import TestParserIntegration
+from tests.switch_cntr_test import TestSwitchContainerParser
 
 
 if __name__ == "__main__":
